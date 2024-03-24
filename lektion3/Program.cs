e0503deed732713105753d5e6de7618f6cac092c
unimatrix27 <sebastian.stuecker@gmail.com>
2024-03-24 14:25:59 +0100

    meinelangweiligeversion

--- lektion3/Program.cs
+++ lektion3/Program.cs
@@ -1,65 +1,74 @@
-﻿class SchoolCalendar
-{
-    private int schoolMonth;
-    private int schoolDay;
-    private int minAge;
-
-    public SchoolCalendar(int sm, int sd, int age)
+﻿class Schulkalender // Klasse erstellt
+{   // Attribute deklarieren (mit int)
+    private int SchulbeginnMonat;
+    private int SchulbeginnTag;
+    private int Mindestalter;
+    // Contruktur (gleicher Name wie Klasse) und dann die drei Werte initialisieren (ohne int). Public machen.
+    public Schulkalender(int sm, int st, int ma)
     {
-        schoolMonth = sm;
-        schoolDay = sd;
-        minAge = age;
+        SchulbeginnMonat = sm;
+        SchulbeginnTag = st;
+        Mindestalter = ma;
     }
-
-    public int GetSchoolDate(int birthdate)
+    // Verhalten der Klasse, also warum ich die überhaupt erstellt habe. Hier soll sie was tun mit Methoden:
+    // 1. Methode (vorher Funktion): Einschulungsdatum, basierend auf einem Geburtsdatum berechnen.
+    public int Einschulungsdatum (int geburtsdatum)
     {
-        int schoolDate = (birthdate / 10000 + minAge) * 10000 + schoolMonth * 100 + schoolDay;
-        if (birthdate % 10000 > schoolMonth * 100 + schoolDay)
-        {
-            schoolDate = schoolDate + 10000;
-        }
-
-        return schoolDate;
-    }
-
-    public int GetSchoolBirthday(int birthdate)
-    {
-        int schoolDate = GetSchoolDate(birthdate);
-        int birthdayYear = schoolDate / 10000;
-        if (birthdate % 10000 < schoolDate % 10000)
-        {
-            birthdayYear = birthdayYear + 1;
-        }
-
-        if (birthdate % 10000 == 2 * 100 + 29 &&
-            !((birthdayYear % 4 == 0 && birthdayYear % 100 != 0) || birthdayYear % 400 == 0))
-        {
-            birthdayYear = (birthdayYear + 3) / 4 * 4;
-            if (birthdayYear % 100 == 0 && birthdayYear % 400 != 0)
-            {
-                birthdayYear = birthdayYear + 4;
-            }
-        }
-
-        int birthday = birthdayYear * 10000 + birthdate % 10000;
-        return birthday;
-    }
-}
-
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        SchoolCalendar calendar1 = new SchoolCalendar(9, 1, 6);
-        int birthdate = 20140229;
-        int schoolDate = calendar1.GetSchoolDate (birthdate);
-        SchoolCalendar calendar2 = new SchoolCalendar(9, 1, 5);
-        int alternateSchoolDate = calendar2.GetSchoolDate(birthdate);
-        int birthday = calendar1.GetSchoolBirthday(birthdate);
+        int einschulungsjahr = geburtsdatum / 10000 + Mindestalter;
         
-        System.Console.WriteLine(schoolDate);
-        System.Console.WriteLine(alternateSchoolDate);
-        System.Console.WriteLine(birthday);
+        // Beispiel: wir haben 1982 und wollen haben 19820801
+        int einschulungstag = einschulungsjahr *10000+ SchulbeginnMonat *100 + SchulbeginnTag;
+        // wenn geburtsTagMonat > einschulungsTagMonat dann +1 Jahr
+        int geburtsTagMonat = geburtsdatum % 10000;
+        int einschulungsTagMonat = einschulungstag % 10000;
+        if (geburtsTagMonat > einschulungsTagMonat)
+        {   // hier + 10000 damit wir plus ein jahr haben wenn wir nach dem einschulugstag geburtstag haben
+            einschulungstag += 10000;
+        }
+        return einschulungstag;
     }
+    // 2. Methode: Wann feiert der Schüler zum ersten Mal seinen Geburtstag in der Schule?
+    public int Schulgeburtstag(int geburtsdatum)
+    {
+        int einschulungsdatum = Einschulungsdatum(geburtsdatum); 
+        int einschulungsTagMonat = einschulungsdatum % 10000;
+        int feierjahr = einschulungsdatum / 10000;
+        
+        int geburtsTagMonat = geburtsdatum % 10000;
+
+         if (geburtsTagMonat < einschulungsTagMonat)
+         {
+             feierjahr += 1;
+         }
+
+         return feierjahr * 10000 + geburtsTagMonat;
+    }
+    
 }
 
+
+
+
+
+
+// hier fängt das Programm an
+class Program
+{
+    public static void Main()
+    {
+        //Deklaration der Variable "meinKalender" mit dem Datentyp "Schulkalender"
+        // Zuweisung eines neuen Schulkalenders zur Variable meinKalender
+        Schulkalender meinKalender = new Schulkalender(8, 1, 5);
+        Schulkalender meinAndererKalender = new Schulkalender(9, 1, 5);
+        int meinEinschulungsDatum = meinKalender.Einschulungsdatum(19770513);
+        int meinAnderesEinschulungsDatum = meinAndererKalender.Einschulungsdatum(19770513);
+
+        int meinErsterSchulgeburtstag = meinKalender.Schulgeburtstag(19770513);
+        int meinAndererErsterSchulgeburtstag = meinAndererKalender.Schulgeburtstag(19770513);
+        
+        Console.WriteLine(meinEinschulungsDatum);
+        Console.WriteLine(meinAnderesEinschulungsDatum);
+        Console.WriteLine(meinErsterSchulgeburtstag);
+        Console.WriteLine(meinAndererErsterSchulgeburtstag);
+    }
+}
